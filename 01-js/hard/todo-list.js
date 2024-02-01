451e07e677d008d2d73e271bde8889e7083d50d3
D5CD1198JYB <D5CD1198JYB@5CD1198JYB.corp.ola.com>
2024-02-01 17:09:20 +0530

    added todo class problem

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,46 @@
 */
 
 class Todo {
-
+  constructor(){
+    this.todos=[];
+    this.id=1;
+  }
+  addTodo(task,desc){
+    this.todos.push({
+      "id":this.id,
+      "task":task,
+      "desc":desc,
+      "completedStatus":false
+    })
+    this.id=this.id+1;
+  }
+  showAllTodo(){
+    console.log(this.todos);
+  }
+  showTodo(id){
+    console.log(this.todos[id-1]);
+  }
+  updateTodo(id,task,desc,status){
+    for(let i=0; i<this.todos.length; i++){
+      if(this.todos[i].id===id){
+        this.todos[i].task=task;
+        this.todos[i].desc=desc;
+        this.todos[i].completedStatus=status;
+      }
+    }
+  }
+  deleteAllTodos(){
+    this.todos=[];
+  }
 }
+
+const todo=new Todo();
+todo.addTodo("code","for 2 hours");
+todo.addTodo("code dsa","for 2 hours");
+todo.showAllTodo();
+todo.updateTodo(2,"code dsaaaa","for 2 hours",true);
+todo.showTodo(2);
+todo.deleteAllTodos();
+todo.showAllTodo();
 
 module.exports = Todo;
