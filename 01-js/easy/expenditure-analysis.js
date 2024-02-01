dc0363f136daf4f1f6f35b61d6c0643163752c23
D5CD1198JYB <D5CD1198JYB@5CD1198JYB.corp.ola.com>
2024-02-01 15:37:53 +0530

    first sol in main

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -9,7 +9,53 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let ans=[];
+  for(let i=0; i<transactions.length; i++){
+    let cat=transactions[i].category;
+    let flag=false;
+    for(let k=0; k<ans.length; k++){
+      if(ans[k].category===cat){
+        flag=true;
+        break;
+      }
+    }
+    if(flag===true){
+      continue;
+    }
+    let total=transactions[i].price;
+    for(let j=i+1; j<transactions.length; j++){
+      if(transactions[j].category===transactions[i].category){
+        total+=transactions[j].price;
+      }
+    }
+    ans.push({"category":cat,"totalSpent":total});
+  }
+  console.log(ans);
+  return ans;
+  // return [];
 }
+calculateTotalSpentByCategory([
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: "Food",
+    itemName: "Pizza",
+  },
+  {
+    id: 2,
+    timestamp: 1656105600000,
+    price: 20,
+    category: "Food",
+    itemName: "Burger",
+  },
+  {
+    id: 3,
+    timestamp: 1656134400000,
+    price: 30,
+    category: "Food",
+    itemName: "Sushi",
+  },
+]);
 
 module.exports = calculateTotalSpentByCategory;
